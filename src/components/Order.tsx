c3d6b62fbb496ec577961fbcd52a6397369b140f
Alex <acbereg@gmail.com>
2023-07-10 22:59:25 +1000

    New

--- src/components/Order.tsx
+++ src/components/Order.tsx
@@ -1,18 +1,27 @@
-import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 import { IOrder } from "../models"
-
 
 interface OrderProps {
     order: IOrder
 }
 export function Order({ order }: OrderProps) {
+    let infType;
+    const date = new Date(order.dateCreate);
+    const customDate = ('0' + date.getDate()).slice(-2) + '.' + ('0' + date.getMonth()).slice(-2) + '.' + date.getFullYear();
+
+    if (order.orderType === 'deal'){
+       infType = <p className='text-success'>{order.orderType}</p>
+    } else {
+       infType = <p className='text-primary'>{order.orderType}</p>
+    }
+
+
     return(
         <div className="col-12 col-sm-6 p-6">
             <div className='p-2 border bg-light d-flex justify-content-between'>
                 <Link to={`/order/${ order.id }`} className="text-start text-decoration-none text-muted">{ order.title }</Link>
-                
-                <p className='m-0'>{ order.createdAt.split('T')[0] }</p>
+                {infType}   
+                <p className='m-0'>{ customDate }</p>
             </div>
         </div>
     )
