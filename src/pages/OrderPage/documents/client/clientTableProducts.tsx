6b2f3f21eeb52e4d01ccf3a1f003d5c29df08d38
Alex <acbereg@gmail.com>
2024-06-23 18:32:07 +1000

    In Clien pdf change vision of total price (total price with delivery)

--- src/pages/OrderPage/documents/client/clientTableProducts.tsx
+++ src/pages/OrderPage/documents/client/clientTableProducts.tsx
@@ -4,12 +4,14 @@ type ClientTableProductsProps = {
 	product: DocTableProduct
 	index: number
 	startIndex: number | undefined
+	delivery: number
 }
 
 export function ClientTableProducts({
 	product,
 	index,
 	startIndex,
+	delivery,
 }: ClientTableProductsProps) {
 	// const total_price = (
 	// 	Number(product.bending) +
@@ -17,14 +19,18 @@ export function ClientTableProducts({
 	// 	Number(product.cut_cost) +
 	// 	Number(product.metal)
 	// ).toFixed(1)
-
 	return (
 		<tr>
 			<td>{startIndex ? startIndex + index + 1 : 0}</td>
 			<td>{product.name}</td>
 			<td>-</td>
 			<td>{product.quantity}</td>
-			<td>{Math.ceil(product.totalPrice)}</td>
+			<td>
+				{Math.ceil(
+					product.totalPrice +
+						delivery * Number(product.weight) * product.quantity
+				)}
+			</td>
 		</tr>
 	)
 }
